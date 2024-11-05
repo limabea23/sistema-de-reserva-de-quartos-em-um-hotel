04900eecade549910a0386b5a7d8f6ef72446f53
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-05 09:26:39 -0300

    create reservas table

--- beatrizlima.sql
+++ beatrizlima.sql
@@ -13,3 +13,14 @@ CREATE TABLE quartos (
     tipo VARCHAR(50) NOT NULL,
     numero INT
 );
+
+CREATE TABLE reservas (
+    id_reserva SERIAL PRIMARY KEY,
+    datainicio DATE NOT NULL,
+    datatermino DATE NOT NULL,
+    hora_reserva TIME NOT NULL,
+    id_hospede INT NOT NULL,
+    id_quarto INT NOT NULL,
+    CONSTRAINT fk_hospede FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
+    CONSTRAINT fk_reserva FOREIGN KEY (id_reserva) REFERENCES reservas(id_reserva)
+);
