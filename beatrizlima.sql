54b8584c90527d489adf3b90e1b631e529d55caa
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-05 09:02:59 -0300

    alter id name

--- beatrizlima.sql
+++ beatrizlima.sql
@@ -3,7 +3,7 @@ CREATE DATABASE reservaquartos;
 \c reservaquartos;
 
 CREATE TABLE hospedes (
-    id_hospedes SERIAL PRIMARY KEY,
+    id_hospede SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
 );
