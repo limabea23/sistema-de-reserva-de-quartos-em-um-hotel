5feb56768632c679d1eee23a6d71bff32a8a3e7b
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-05 09:01:27 -0300

    create hospedes table

--- beatrizlima.sql
+++ beatrizlima.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE reservaquartos;
+
+\c reservaquartos;
+
+CREATE TABLE hospedes (
+    id_hospedes SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+);
